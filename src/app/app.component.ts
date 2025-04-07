3bba647cd349c2d0ea7efa19e20cc37c67bd38e0
vgmil <vgmiltonisaac@gmail.com>
2025-04-07 12:04:31 -0500

    Initial Commit AR Works

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  standalone: true,
+})
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    
+  }
+}
